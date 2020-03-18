7dbba6df0b1dd63029195f4255d4eeafca4f3d14
Baraa Halabi <baraaelhalabi@gmail.com>
2020-03-18 22:10:22 +0200

    Add files via upload

---
+++ EhAb AnD gCd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T, Number;
+    cin >> T;
+    while (T --) {
+        cin >> Number;
+        cout << 1 << " " << Number - 1 << endl;
+    }
+}
